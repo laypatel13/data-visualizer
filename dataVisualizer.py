3f3b2964de897d6dec97f54e106fd7614f80d3b5
laypatel13 <laypatel1322008@gmail.com>
2026-04-30 23:42:17 +0530

    merge them all with main function

--- dataVisualizer.py
+++ dataVisualizer.py
@@ -30,3 +30,38 @@ def plot_profit_vs_sales(df):
     sns.scatterplot(x="sales", y="profit", data=df)
     plt.title("Profit vs Sales")
     plt.show()
+
+def show_menu():
+    print("\n----Data Visualizer----")
+    print("1.Sales over time")
+    print("2.Sales by product")
+    print("3.Profit vs Sales")
+    print("4.Exit")
+
+
+def main():
+    file_path = input("Enter CSV file path: ")
+
+    df = load_data(file_path)
+
+    if df is None:
+        return
+
+    while True:
+        show_menu()
+        choice = input("Select an option (1-4): ")
+
+        if choice == "1":
+            plot_sales_over_time(df)
+        elif choice == "2":
+            plot_sales_by_product(df)
+        elif choice == "3":
+            plot_profit_vs_sales(df)
+        elif choice == "4":
+            print("Exiting...")
+            break
+        else:
+            print("Invalid choice. Please select 1-4.")
+
+
+main()
